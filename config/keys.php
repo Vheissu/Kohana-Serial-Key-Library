9657219d9449911420606f75faca23e71f7fe230
Kerstens Maxim <admin@happydemon.org>
2013-08-05 20:13:55 +0200

    config does not require 'keys.' prefix
    Kohana's config reader will recognise the 'keys.' prefix based on the config file's name. Kohana::$config->load('keys') would load this file and Kohana::$config->load('keys.default_format') would load only the default_format value of this config file

--- config/keys.php
+++ config/keys.php
@@ -3,18 +3,18 @@
 return array(
 
     // If we no key format is specified, used this one
-    'keys.default_format' => 'XXXX-XXXX-LOLL-ROFL',
+    'default_format' => 'XXXX-XXXX-LOLL-ROFL',
 
     // We can construct formatted keys by specifying a character to replace
-    'keys.replace_char' => 'X',
+    'replace_char' => 'X',
 
     // Should all letters in generated key be capitals only?
-    'keys.all_caps' => TRUE,
+    'all_caps' => TRUE,
 
     // Force generated key to only use lowercase letters
-    'keys.all_lowercase' => FALSE,
+    'all_lowercase' => FALSE,
 
     // Should the generated key only contain numeric values?
-    'keys.numeric_only' => FALSE,
+    'numeric_only' => FALSE,
 
 );
